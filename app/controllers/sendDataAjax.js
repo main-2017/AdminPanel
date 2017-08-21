bd29f5542516dd95b85c1b031b8051e9f93585c4
main-2017 <contacto@main-group.net>
2017-08-21 16:58:52 -0300

    Edición de slider terminada

--- app/controllers/sendDataAjax.js
+++ app/controllers/sendDataAjax.js
@@ -24,3 +24,56 @@ $(document).on('submit', '#deleteSlider', function(event){
 	});
 	
 });
+
+//Pasaje de datos a ventana modal
+$(document).on('click', '.editModal', function(){
+	var eID = $(this).val();
+	$(".modal-body #id-slider").val(eID);
+	loadEditModal(eID);
+});
+
+// Carga de datos en Formulario Modal
+function loadEditModal(input){
+	jQuery.ajax({
+		url: '../../../app/controllers/editSlider.php',
+		type: 'POST',
+		dataType: 'html',
+		data: {input: input},
+	})
+	.done(function(serverAnswer) {
+		$("#formEditSlider").html(serverAnswer);
+	})
+	.fail(function() {
+		console.log("error");
+	})
+};
+
+//Guardado de cambios en edición de Slider
+
+$(document).on('submit', '#formEditSlider', function(event){
+	event.preventDefault();
+	jQuery.ajax({
+	  url: '../../../app/controllers/updateEditSlider.php',
+	  type: 'POST',
+	  dataType: 'json',
+	  data: $(this).serialize(),
+	  complete: function(serverRespond) {
+	    if (serverRespond.error) {
+	    	$('#error').slideDown('slow', function(){
+	    	$(this).slideUp(3000);
+	    });
+	    }else{
+	    	$('#success').slideDown('slow', function(){
+	    	$(this).slideUp(3000);
+	    	});
+	    }
+	  },
+	  success: function(textStatus) {
+	    	console.log(textStatus);
+	  },
+	  error: function(xhr, textStatus, errorThrown) {
+	    //called when there is an error
+	  }
+	});
+	
+});
--- resources/components/modals.php
+++ resources/components/modals.php
@@ -64,3 +64,29 @@
   </div>
 </div>
 </div>
+
+<!-- Modal de edicion de imagenes -->
+<div id="editSlider" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header blue-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Editar imágenes</h4>
+      </div>
+      <div id="success" class="alert alert-success text-center" style="display: none;">Datos actualizados correctamente.</div>
+      <div id="error" class="alert alert-danger text-center" style="display: none;">Ocurrió un error durante la operación. Vuelva a intentarlo</div>
+      <div class="modal-body">
+        <form role="form" name="formEditSlider" id="formEditSlider" method="POST" accept-charset="utf-8">
+          <div class="form-group">
+            <input type="number" name="ID" id="id-slider" value class="form-control">
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+        <button type="submit" form="formEditSlider" class="btn btn-success">Guardar</button>
+      </div>
+</div>
+</div>
+</div>
